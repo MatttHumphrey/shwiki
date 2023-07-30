136d73563fa902c5784f6c2656e7bfa14bc14543
Matt Humphrey <Matthew.Humphrey04@outlook.com>
2023-07-30 15:16:19 +0100

    Adding remove_list directly to area_dict.py

--- scripts/utils/area_dict.py
+++ scripts/utils/area_dict.py
@@ -1,14 +1,15 @@
-from scripts.utils.read_i2 import read_i2
-from scripts.utils.read_gde import read_gde
+from read_i2 import read_i2
+from read_gde import read_gde
 
 def area_dict():
     descs = read_i2()
     data = read_gde()
     areas = {}
+    remove_list = ['',"winterfair2022_shack","winterfair2022_icerink","winterfair2022_sledding","winterfair2022_ferriswheel","winterfair2022_train"]
     for line in data:
         if data[line].get("1071") == "Quest":
             area_key = data[line].get("20", "").lower()
-            if area_key and area_key not in areas:
+            if area_key and area_key not in areas and area_key not in remove_list:
                 area_desc_key = f"namekey_{area_key}" if f"namekey_{area_key}" in descs else f"questtitle_{area_key}"
                 area_desc = descs.get(area_desc_key, "").lower()
                 areas[area_key] = area_desc
