947899b9bf2878e931d753be0c3fc1df7de95933
Matt Humphrey <Matthew.Humphrey04@outlook.com>
2024-02-18 10:33:00 +0000

    Turns out I broke this a while back. Fixed now!

--- scripts/area_list.py
+++ scripts/area_list.py
@@ -1,9 +1,12 @@
 from .utils.match_location import area_dict
 from .utils.output_file import output_file
+from .utils.read_data import read_gde, read_i2
 
 def area_list():
     '''Generates a list of all in file area names.'''
-    area_lists = list(area_dict().keys())
+    gde_data = read_gde()
+    i2_data = read_i2()
+    area_lists = list(area_dict(gde_data, i2_data).keys())
     with open(output_file("area_output.txt"), "w", encoding="utf8") as output:
         output.writelines("\n".join(area_lists))
     print("Action completed.")
