8abf43918a37207a1dd419485dca3aa64882a44d
Matt Humphrey <Matthew.Humphrey04@outlook.com>
2024-03-14 19:32:51 +0000

    Updated functions list to use dictionary

--- shwiki.py
+++ shwiki.py
@@ -14,18 +14,18 @@ from scripts.task_list import task_list
 from scripts.utils.match_location import match_location
 from scripts.utils.match_plant import match_plant
 
-functions = [
-    (area_dialogue, [("location", str), ("upload", bool)], "Outputs the dialogue spoken in a given location."),
-    (area_items, [("location", str), ("upload", bool)], "Outputs a list of all items needed for a given location."),
-    (area_list, [], "Outputs a list of filenames for all locations in game."),
-    (botanical_plant, [("plant_name", str), ("filename", str), ("upload", bool)], "Outputs an Endangered Plant wiki page."),
-    (full_dialogue, [], "Outputs the dialogue spoken across the whole game."),
-    (game_items, [("upload", bool)], "Outputs a list of all items needed across the whole game."),
-    (hard_items, [("upload", bool)], "Outputs a list of hard items needed for all areas in the game. Uses scripts/utils/hard_items.py to determine which items to include."),
-    (task_list, [("location", str), ("loc_id", str), ("upload", bool)], "Outputs a list of tasks for a given location, with tasks named after the loc_id provided."),
-    (area_page, [("location", str), ("loc_id", str), ("upload", bool)], "Outputs the dialogue, task list and area page for a given location."),
-    (excel_task_list, [], "Outputs a spreadsheet containing all tasks across the game.")
-]
+functions = {
+    "area_dialogue": (area_dialogue, [("location", str), ("upload", bool)], "Outputs the dialogue spoken in a given location."),
+    "area_items": (area_items, [("location", str), ("upload", bool)], "Outputs a list of all items needed for a given location."),
+    "area_list": (area_list, [], "Outputs a list of filenames for all locations in game."),
+    "botanical_plant": (botanical_plant, [("plant_name", str), ("filename", str), ("upload", bool)], "Outputs an Endangered Plant wiki page."),
+    "full_dialogue": (full_dialogue, [], "Outputs the dialogue spoken across the whole game."),
+    "game_items": (game_items, [("upload", bool)], "Outputs a list of all items needed across the whole game."),
+    "hard_items": (hard_items, [("upload", bool)], "Outputs a list of hard items needed for all areas in the game. Uses scripts/utils/hard_items.py to determine which items to include."),
+    "task_list": (task_list, [("location", str), ("loc_id", str), ("upload", bool)], "Outputs a list of tasks for a given location, with tasks named after the loc_id provided."),
+    "area_page": (area_page, [("location", str), ("loc_id", str), ("upload", bool)], "Outputs the dialogue, task list and area page for a given location."),
+    "excel_task_list": (excel_task_list, [], "Outputs a spreadsheet containing all tasks across the game.")
+}
 
 def create_parser():
     '''Creates the parser from the functions list above.'''
@@ -34,14 +34,14 @@ def create_parser():
     parser.add_argument("-h", "--help", action = "store_true", help = "Shows this help message.")
     subparsers = parser.add_subparsers(title = "subcommands", dest = "subcommand")
 
-    for func, args, help in functions:
+    for func, args, help in functions.values():
         parser_func = subparsers.add_parser(func.__name__, help = help)
         parser_func.set_defaults(func = func)
         parser_help = {"upload": "Flag to upload page straight to wiki. Otherwise writes a text file to the output folder.",
                        "location": "Location to run the function on. Accepts both in game and file names of areas.",
                        "loc_id": "Key used to name tasks in task lists.",
                        "plant_name": "Endangered plant to run the function on. Accepts both in game and file names of plants.",
-                       "filename": "Wiki filename of endangered plant."
+                       "filename": "Wiki image filename of endangered plant."
                        }
 
         for arg, arg_type in args:
@@ -63,22 +63,20 @@ def main():
         parser.print_help()
         return
 
-    for func, argspec, help in functions:
-        if args.subcommand == func.__name__:
+    (func, argspec, help) = functions.get(args.subcommand)
+    if func:
+        func_args = {}
+        for arg, arg_type in argspec:
+            value = getattr(args, arg)
+            if arg == "location":
+                value = match_location(value)
+            if arg == "plant_name":
+                value = match_plant(value)
+            func_args[arg] = value
 
-            func_args = {}
-            for arg, arg_type in argspec:
-                value = getattr(args, arg)
-                if arg == "location":
-                    value = match_location(value)
-                if arg == "plant_name":
-                    value = match_plant(value)
-                func_args[arg] = value
-
-            func(**func_args)
-            return
-
-    print(f"Invalid subcommand: {args.subcommand}. Use -h for help.")
+        func(**func_args)
+    else:
+        print(f"Invalid subcommand: {args.subcommand}. Use -h for help.")
 
 if __name__ == "__main__":
     main()
